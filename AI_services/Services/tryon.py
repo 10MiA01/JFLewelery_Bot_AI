0f2877104a8969696bbf459a8cdac6492d6c8512
10MiA01 <miklashevichanastasiyachelovek@gmail.com>
2025-07-19 17:54:49 +0200

    Face analizys- done + reading png from folders

--- AI_services/Services/tryon.py
+++ AI_services/Services/tryon.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import math
+from pathlib import Path
 from Helpers import CategoryTryOnDict
 
 mp_pose = mp.solutions.pose
@@ -122,9 +123,12 @@ async def process_image(file: UploadFile, category: str, id: int):
     image = Image.open(file.file).convert("RGB")
     cv2_image = pil_to_cv2(image)
 
-    # To Do
     #Get the inamge of product by id
-    overlay_image = cv2.imread(f"products/{id}.png", cv2.IMREAD_UNCHANGED)
+    product_path = Path(__file__).parent.parent / "JFJewelery" / "Media" / "images" / "products" / str(id) / "tryon.png"
+    overlay_image = cv2.imread(str(product_path), cv2.IMREAD_UNCHANGED)
+
+    # Declare the output
+    output_image = None
 
     # Get the category
     selected_group = find_group_by_category(category)
@@ -179,44 +183,148 @@ async def process_image(file: UploadFile, category: str, id: int):
                 output_left = overlay_image_alpha(cv2_image, transform_left, x_left, y_left)
                 output_right = overlay_image_alpha(output_left, transform_right, x_right, y_right)
 
-                return output_right
+                output_image = output_right
 
-            elif category == "Necklaces":           # Landmark-152
-                x = int(landmarks[152].x * w)
-                y = int(landmarks[152].y * h)
+            elif category == "Necklaces": 
+                x1 = int(landmarks[152].x * w)        # Landmark chin-152
+                y1 = int(landmarks[152].y * h)
+                x2 = int(landmarks[200].x * w)       # Landmark chin-152
+                y2 = int(landmarks[200].y * h)
 
-                # image.paste(necklace_img, (x, y), necklace_img)
+                # coordinates
+                p1 = (x1, y1)
+                p2 = (x2, y2)
 
-            elif category == "Chokers":             # Landmark-17
-                x = int((landmarks[17].x + landmarks[152].x) / 2 * w)
-                y = int((landmarks[17].y + landmarks[152].y) / 2 * h)
+                # angles
+                angle = get_angle(p1, p2)
 
-                # image.paste(choker_img, (x, y), choker_img)
-
-
-            elif category == "Pendants":            # Landmark-152
-                x = int(landmarks[152].x * w)
-                y = int((landmarks[152].y + 0.05) * h)  # Little lower on the neck
-
-                # image.paste(pendant_img, (x, y), pendant_img)
-
-
-            elif category == "Hair_Accessories":            # Landmark-10
-                x = int(landmarks[10].x * w)
-                y = int((landmarks[10].y - 0.05) * h)  # little up, in hair
-
-                # image.paste(hair_img, (x, y), hair_img)
-
-            elif category == "Ear_Cuffs":                # Landmark-234 left, 454 right
-                x_left = int(landmarks[234].x * w)       # Left ear
-                y_left = int(landmarks[234].y * h)
-                x_right = int(landmarks[454].x * w)       # Right ear
-                y_right = int(landmarks[454].y * h)
+                # scale
+                scale = get_scale(p1, p2, w)
+                
+                # transform
+                transform = rotate_and_scale_image(overlay_image, angle, scale)
 
                 # paste the image
+                output = overlay_image_alpha(cv2_image, transform, x2, y2)
+
+                output_image = output
+
+            elif category == "Chokers":            
+                x1 = int(landmarks[200].x * w)  # Left neck point
+                y1 = int(landmarks[200].y * h)
+                x2 = int(landmarks[430].x * w)  # Right neck point
+                y2 = int(landmarks[430].y * h)
+                x_center = (x1 + x2) // 2       # Center of the neck
+                y_center = (y1 + y2) // 2
+
+                # coordinates
+                p1 = (x1, y1)
+                p2 = (x2, y2)
+
+                # angles
+                angle = get_angle(p1, p2)
+                
+                # scale
+                scale = get_scale(p1, p2, w)
+                
+                # transform
+                transform = rotate_and_scale_image(overlay_image, angle, scale)
+
+                # paste the image
+                output = overlay_image_alpha(cv2_image, transform, x_center, y_center)
+
+                output_image = output
+
+            elif category == "Pendants":            # Landmark-152
+                x1 = int(landmarks[152].x * w)
+                y1 = int(landmarks[152].y * h)
+                x2 = x1
+                y2 = int((landmarks[152].y + 0.05) * h)  # Little lower on the neck
+
+                # coordinates
+                p1 = (x1, y1)
+                p2 = (x2, y2)
+
+                # angles
+                angle = get_angle(p1, p2)
+                
+                # scale
+                scale = get_scale(p1, p2, w)
+                
+                # transform
+                transform = rotate_and_scale_image(overlay_image, angle, scale)
+
+                # paste the image
+                output = overlay_image_alpha(cv2_image, transform, x2, y2)
+
+                output_image = output
+
+            elif category == "Hair_Accessories":            # Landmark-10
+                x1 = int(landmarks[10].x * w)
+                y1 = int((landmarks[10].y - 0.05) * h)  # little up, in hair
+                x2 = x1
+                y2 = int(landmarks[152].y * h)
+
+                # coordinates
+                p1 = (x1, y1)
+                p2 = (x2, y2)
+
+                # angles
+                angle = get_angle(p1, p2)
+                
+                # scale
+                scale = get_scale(p1, p2, w)
+                
+                # transform
+                transform = rotate_and_scale_image(overlay_image, angle, scale)
+
+                # paste the image
+                output = overlay_image_alpha(cv2_image, transform, x1, y1)
+                
+                output_image = output
+
+            elif category == "Ear_Cuffs":                # Landmark-234 left, 454 right
+                # Left ear
+                x_left = int(landmarks[234].x * w)
+                y_left = int(landmarks[234].y * h)
+                x_left_up = int(landmarks[127].x * w)
+                y_left_up = int(landmarks[127].y * h)
+                x_left_center = (x_left + x_left_up) // 2       # Center of the ear
+                y_left_center = (y_left + y_left_up) // 2
+
+                # Right ear
+                x_right = int(landmarks[454].x * w)
+                y_right = int(landmarks[454].y * h)
+                x_right_up = int(landmarks[356].x * w)
+                y_right_up = int(landmarks[356].y * h)
+                x_right_center = (x_right + x_right_up) // 2
+                y_right_center = (y_right + y_right_up) // 2
 
 
-        #analyze face 
+                # coordinates
+                p1_left = (x_left_up, y_left_up)
+                p2_left = (x_left, y_left)
+                p1_right = (x_right_up, y_right_up)
+                p2_right = (x_right, y_right)
+
+                # angles
+                angle_left = (p1_left, p2_left)
+                angle_right = (p1_right, p2_right)
+
+                # scale
+                scale_left =(p1_left, p2_left, w)
+                scale_right = (p1_right, p2_right, w)
+
+                # transform
+                transform_left = rotate_and_scale_image(overlay_image, angle_left, scale_left)
+                transform_right = rotate_and_scale_image(overlay_image, angle_right, scale_right)
+
+                # paste the image
+                output_left = overlay_image_alpha(cv2_image, transform_left, x_left_center, y_left_center)
+                output_right = overlay_image_alpha(output_left, transform_right, x_right_center, y_right_center)
+
+                output_image = output_right
+        
         
 
 
